3c7b2d22b95a892c933c76866f7aa744b33c1ff0
Sönke Liebau <soenke.liebau@stackable.tech>
2025-12-06 22:03:14 +0100

    Added initial version of Airflow which may or may not be pulled from the in-cluster clone of this repo by argocd..

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 kind delete cluster
 kind create cluster
-stackablectl -s /home/sliebau/IdeaProjects/openmetadata-demo/openmetadata-demo/stack/stack.yaml  stack install forgejo
-cat stack/forgejo.yaml | yq .options | helm install forgejo oci://code.forgejo.org/forgejo-helm/forgejo -f -
+stackablectl -s /home/sliebau/IdeaProjects/openmetadata-demo/openmetadata-demo/infrastructure/stack.yaml  stack install forgejo
+cat infrastructure/forgejo.yaml | yq .options | helm install forgejo oci://code.forgejo.org/forgejo-helm/forgejo -f -
