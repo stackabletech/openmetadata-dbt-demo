d5343dfebfbcd80f103ece6c7c320bc4434bdaa8
Sönke Liebau <soenke.liebau@stackable.tech>
2025-12-10 22:47:27 +0100

    next try to fix stuff..

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 kind delete cluster
 kind create cluster
 stackablectl -s /home/sliebau/IdeaProjects/openmetadata-demo/openmetadata-demo/infrastructure/stack.yaml  stack install forgejo
-cat infrastructure/forgejo.yaml | yq .options | helm install forgejo oci://code.forgejo.org/forgejo-helm/forgejo -f -
+#cat infrastructure/forgejo.yaml | yq .options | helm install forgejo oci://code.forgejo.org/forgejo-helm/forgejo -f -
