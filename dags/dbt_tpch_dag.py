48a1018ff773475ac2f54246f7de9ce735aa2fda
Sönke Liebau <soenke.liebau@stackable.tech>
2026-03-13 17:31:41 +0100

    Changed load mode to avoid needing dbt on scheduler image

--- dags/dbt_tpch_dag.py
+++ dags/dbt_tpch_dag.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from pathlib import Path
 
 from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig, RenderConfig
-from cosmos.constants import ExecutionMode
+from cosmos.constants import ExecutionMode, LoadMode
 
 DBT_PROJECT_PATH = Path("/stackable/app/git-0/current/dags/dbt/tpch_demo")
 
@@ -18,6 +18,9 @@ dbt_tpch_dag = DbtDag(
         profiles_yml_filepath=DBT_PROJECT_PATH / "profiles.yml",
         profile_name="tpch_demo",
         target_name="dev",
+    ),
+    render_config=RenderConfig(
+        load_method=LoadMode.CUSTOM,
     ),
     execution_config=ExecutionConfig(
         execution_mode=ExecutionMode.VIRTUALENV,
