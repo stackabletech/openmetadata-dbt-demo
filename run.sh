d76afbc60487e35106d3f7a4c2d415152080d3e6
Sönke Liebau <soenke.liebau@stackable.tech>
2026-03-16 15:11:14 +0100

    Add TrinoCatalog to reference LakeKeeper and GarageFS

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-kind delete cluster
-kind create cluster
-stackablectl -s /home/sliebau/IdeaProjects/openmetadata-demo/openmetadata-demo/infrastructure/stack.yaml  stack install forgejo
-#cat infrastructure/forgejo.yaml | yq .options | helm install forgejo oci://code.forgejo.org/forgejo-helm/forgejo -f -
